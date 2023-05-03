3061682f63e270155e08357877806021ff1e536c
Jun <junyuplee@mines.edu>
2023-05-03 10:06:44 -0600

    Initial Commit

---
+++ SpaceGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SpaceGame
+//
+//  Created by Junyup Lee on 5/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
